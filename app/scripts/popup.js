e46877d17a11c096e78a4fe2c4a872f2f9e94c69
Andrew Peter Prifer <prifer.andras@gmail.com>
2017-01-14 03:24:47 +0100

    Add expandable list elements

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -4,13 +4,25 @@ import popup from 'raw-loader!./templates/content.hbs';
 
 $('#root').text('Loading...');
 
-chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
   chrome.tabs.sendMessage(tabs[0].id, {}, response => {
-    console.log('log '+mustache.render(popup, { analysis: response.analysis }))
+    const analysis = response.analysis;
+    let clear = true;
+
+    for (let i in analysis) {
+      if (analysis[i] === true) {
+        clear = false;
+        break;
+      }
+    }
 
     // timeout to fix incorrect popup size
     setTimeout(() => {
-      $('#root').html(mustache.render(popup, { analysis: response.analysis }));
+      $('#root').html(mustache.render(popup, { analysis: response.analysis, clear }));
+      $('.expander-trigger').click(function () {
+        console.log('toggle');
+        $(this).toggleClass("expander-hidden");
+      });
     }, 200);
   });
 });
