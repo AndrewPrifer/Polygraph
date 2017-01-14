b54d892d36d3873c8223b36f1c159ad2534027fb
Andrew Peter Prifer <prifer.andras@gmail.com>
2017-01-14 03:23:45 +0100

    Improve pointing marker

--- app/scripts/analyzer.js
+++ app/scripts/analyzer.js
@@ -10,11 +10,12 @@ export default function analyze(title) {
     listicle: false,
   };
 
+  // TODO: add whitespace around all tokens
   analysis.emotion = /you/i.test(title);
   analysis.opinion = /should|need/i.test(title);
   analysis.quote = /"[^"]*"|'[^']*'/.test(title);
   analysis.uncertainty = /might|may|maybe|probably|certanly|\?$/i.test(title);
-  analysis.pointing = /this|here/i.test(title);
+  analysis.pointing = /this|these|( |^)here/i.test(title);
   analysis.listicle = /(^|the )\d+/i.test(title);
   analysis.exaggeration = /total|fucking|garantee|literally|awesome|!$/i.test(title);
 
