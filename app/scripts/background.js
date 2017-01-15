0a94b96e5b2b01db34ab8f45e3f17a13199c745e
Andrew Peter Prifer <prifer.andras@gmail.com>
2017-01-15 15:22:58 +0100

    Set icon with appropriate size

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -15,41 +15,53 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
   }
 
-  setPageActionIcon(count, 19, tab);
-  chrome.pageAction.show(tab);
+  const icons = [16, 19, 38, 128].map(e => drawIcon(count, e, tab));
+
+  Promise.all(icons).then(values => {
+    chrome.pageAction.setIcon({
+      imageData: {
+        '16': values[0],
+        '19': values[1],
+        '38': values[2],
+        '128': values[3],
+      },
+      tabId: tab,
+    });
+    chrome.pageAction.show(tab);
+  });
 });
 
-function setPageActionIcon(badgeText, size, tab) {
+function drawIcon(badgeText, size, tab) {
   const canvas = document.createElement('canvas');
   const img = document.createElement('img');
-  const imgScale = 0.8;
-  const badgeRatio = 0.5;
+  const imgScale = 0.9;
+  const badgeRatio = 0.7;
 
   const badgeX = size * (1 - badgeRatio);
   const badgeY = badgeX;
   const badgeSize = size * badgeRatio;
-  img.onload = function () {
-    const context = canvas.getContext('2d');
 
-    const imgSize = size * imgScale;
-    const imgOffset = (size - imgSize)/2;
-    context.drawImage(img, imgOffset, imgOffset, imgSize, imgSize);
+  return new Promise((resolve, reject) => {
+    img.onload = function () {
+      const context = canvas.getContext('2d');
 
-    context.fillStyle = "rgba(255,0,0,1)";
-    const radius = badgeSize / 2;
-    context.arc(badgeX + radius, badgeY + radius, radius, 0, 2 * Math.PI, false);
-    context.fill();
+      const imgSize = size * imgScale;
+      const imgOffset = (size - imgSize) / 2;
+      context.drawImage(img, imgOffset, imgOffset, imgSize, imgSize);
 
-    context.fillStyle = "white";
-    context.font = `${badgeSize}px Arial`;
-    context.textAlign = "center"
-    context.textBaseline = "middle";
-    context.fillText(`${badgeText}`, badgeX + radius, badgeY + radius);
+      context.fillStyle = "rgba(255,0,0,1)";
+      const radius = badgeSize / 2;
+      context.arc(badgeX + radius, badgeY + radius, radius, 0, 2 * Math.PI, false);
+      context.fill();
 
-    chrome.pageAction.setIcon({
-      imageData: context.getImageData(0, 0, size, size),
-      tabId: tab,
-    });
-  };
-  img.src = "../images/icon-19.png";
+      context.fillStyle = "white";
+      context.font = `${badgeSize}px Arial`;
+      context.textAlign = "center"
+      context.textBaseline = "middle";
+      context.fillText(`${badgeText}`, badgeX + radius, badgeY + radius);
+
+      resolve(context.getImageData(0, 0, size, size));
+    };
+    img.src = `../images/icon-${size}.png`;
+  });
 }
