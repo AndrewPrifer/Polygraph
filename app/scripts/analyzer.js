6290df5362d7003a55be5fb399bac6690b6da348
Andrew Peter Prifer <prifer.andras@gmail.com>
2017-01-15 23:50:36 +0100

    Fix question mark detection

--- app/scripts/analyzer.js
+++ app/scripts/analyzer.js
@@ -12,7 +12,7 @@ export default function analyze(title) {
 
   analysis.emotion = word(/you|your/i).test(title);
   analysis.quote = word(/"[^"]*"|'[^']*'/).test(title);
-  analysis.uncertainty = word(/might|may|maybe|probably|certanly|\?$/i).test(title);
+  analysis.uncertainty = word(/might|may|maybe|probably|certanly/i).test(title) || /\?$/.test(title);
   analysis.omission = word(/this|these|here/i).test(title);
   analysis.listicle = word(/(^|the )\d+/i).test(title);
   analysis.exaggeration = word(/total|fucking|garantee|literally|awesome|!$/i).test(title);
