c5cc551a8522ab3b6a31e4260723a8217d669564
Andrew Peter Prifer <prifer.andras@gmail.com>
2017-01-15 21:06:53 +0100

    Only match whole words

--- app/scripts/analyzer.js
+++ app/scripts/analyzer.js
@@ -11,13 +11,20 @@ export default function analyze(title) {
   };
 
   // TODO: add whitespace around all tokens
-  analysis.emotion = /you/i.test(title);
-  analysis.opinion = /should|need/i.test(title);
-  analysis.quote = /"[^"]*"|'[^']*'/.test(title);
-  analysis.uncertainty = /might|may|maybe|probably|certanly|\?$/i.test(title);
-  analysis.omission = /this|these|here/i.test(title);
-  analysis.listicle = /(^|the )\d+/i.test(title);
-  analysis.exaggeration = /total|fucking|garantee|literally|awesome|!$/i.test(title);
+  analysis.emotion = word(/you/i).test(title);
+  analysis.opinion = word(/should|need/i).test(title);
+  analysis.quote = word(/"[^"]*"|'[^']*'/).test(title);
+  analysis.uncertainty = word(/might|may|maybe|probably|certanly|\?$/i).test(title);
+  analysis.omission = word(/this|these|here/i).test(title);
+  analysis.listicle = word(/(^|the )\d+/i).test(title);
+  analysis.exaggeration = word(/total|fucking|garantee|literally|awesome|!$/i).test(title);
 
   return analysis;
+}
+
+/**
+ * Ensures that only whole words are matched.
+ */
+function word(pattern) {
+  return new RegExp(`([^a-z]|^)(${pattern.source})([^a-z]|$)`, pattern.flags);
 }
